9624a642b55a4687e84ab6c3b957ce5adb35d098
JC <juanc.vivasm@gmail.com>
2023-05-20 15:55:27 +0000

    Deploy frontend to S3

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,3 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+# Replace the [udagram-test01] below with the S3 bucket name
+aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-363605384631/
+# The trailing / at the end of the URL is important. 
